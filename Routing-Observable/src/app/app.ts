736c8a5398f204e68ecabec9aebc05107f91406a
Prince Mattheus Villanueva <villanueva.prince@iisgalvanimi.edu.it>
2026-09-17 09:57:44 +0000

    aggiunta vettore animali

--- Routing-Observable/src/app/app.ts
+++ Routing-Observable/src/app/app.ts
@@ -1,8 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
